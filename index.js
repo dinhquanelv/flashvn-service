d56044f838f43543b70eb5d0603eb0cdbac6f5d6
Dinh Quan <quanvonguyendinh@gmail.com>
2025-04-29 10:28:28 +0700

    feat: init project

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// apply library
+app.use(
+  cors({
+    origin: process.env.BASE_URL || 'http://localhost:3000', // port fe
+    credentials: true, // allow call api
+  }),
+);
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.head('/ping', (req, res) => {
+  res.sendStatus(200);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
