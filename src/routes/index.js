94fd049736bf28d3e1a9319d3ef0d29c3264aea0
Dinh Quan <quanvonguyendinh@gmail.com>
2025-05-05 21:17:52 +0700

    feat: set up routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,18 @@ const siteRouter = require('./site.routes');
 const authRouter = require('./auth.routes');
 const cardRouter = require('./card.routes');
 const bingoRouter = require('./bingo.routes');
+const futureTellerRouter = require('./futureTeller.routes');
+const raceRouter = require('./race.routes');
+
+const apiVersion = 'v1';
 
 const routes = (app) => {
-  app.use('/api/v1/auth', authRouter);
-  app.use('/api/v1/card', cardRouter);
-  app.use('/api/v1/bingo', bingoRouter);
-  app.use('/', siteRouter);
+  app.use(`/api/${apiVersion}/auth`, authRouter);
+  app.use(`/api/${apiVersion}/card`, cardRouter);
+  app.use(`/api/${apiVersion}/bingo`, bingoRouter);
+  app.use(`/api/${apiVersion}/future-teller`, futureTellerRouter);
+  app.use(`/api/${apiVersion}/race`, raceRouter);
+  app.use(`/`, siteRouter);
 };
 
 module.exports = routes;
